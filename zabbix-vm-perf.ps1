9cb1f5e2cf3ff840dffc487c966e8e779194b64b
Rustavy Zhigulin <zro@mail.ru>
2019-06-26 14:04:01 +0300

    clear vars

--- zabbix-vm-perf.ps1
+++ zabbix-vm-perf.ps1
@@ -12,17 +12,16 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
 #>
 
-
 param(
-	[Parameter(Mandatory=$False)]
-	[string]$QueryName,
-	[string]$VMName,
-	[string]$VMObject
+    [Parameter(Mandatory=$False)]
+    [string]$QueryName,
+    [string]$VMName,
+    [string]$VMObject
 )
+
+Clear-Variable hostname,From,To,encFrom,encTo,bytes,colItems,n,ItemType,Results,objItem -ErrorAction SilentlyContinue
 
 $hostname = Get-WmiObject win32_computersystem | Select-Object -ExpandProperty name
 
